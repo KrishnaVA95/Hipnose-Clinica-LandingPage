afde04baf4b588a460a9c6bd6a0edd23007d1032
Krishna Almeida Varela <krishnavarela.a@gmail.com>
2022-04-08 19:55:08 -0300

    ATT Slider

--- JS/mobile-navbar.js
+++ JS/mobile-navbar.js
@@ -1,3 +1,9 @@
+
+
+//------------------Mobile NavBar-----------------
+
+
+
 class MobileNavbar {
     constructor (mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
@@ -6,6 +12,8 @@ class MobileNavbar {
         this.activeClass = "active";
         this.handleClick = this.handleClick.bind(this);
     }
+
+
 
     animateLinks() {
         this.navLinks.forEach((link, index) => {
@@ -17,6 +25,8 @@ class MobileNavbar {
         });
       }
 
+
+      
 
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
@@ -46,3 +56,88 @@ const mobileNavbar = new MobileNavbar(
 
 mobileNavbar.init();
 
+//---------------Slides----------------------------
+
+const slider = document.querySelector(".slider")
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+
+
+
+nextBtn.addEventListener("click", () => {
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+    if (slideNumber > (numberOfSlides - 1))  {
+        slideNumber = 0;
+    }
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
+
+
+
+prevBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber--;
+    if (slideNumber < 0)  {
+        slideNumber = numberOfSlides - 1;
+    }
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
+
+
+
+var playSlider;
+var repeater = () => {
+    playSlider = setInterval(function(){
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+    
+        slideIcons.forEach((slideIcon) => {
+            slideIcon.classList.remove("active");
+        });
+    
+        slideNumber++;
+        if (slideNumber > (numberOfSlides - 1))  {
+            slideNumber = 0;
+        }
+        slides[slideNumber].classList.add("active");
+        slideIcons[slideNumber].classList.add("active");
+    }, 4000);
+}
+
+repeater();
+
+
+
+slider.addEventListener("mouseover", () => {
+    clearInterval(playSlider);
+});
+
+
+
+slider.addEventListener("mouseout", () => {
+    repeater();
+});
